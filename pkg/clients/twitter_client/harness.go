b7686f4327ed11cbb49858c9876e8345b64de234
trillnye <8289478+trillnye@users.noreply.github.com>
2020-05-03 02:15:36 -0400

    Add smart route tests

--- pkg/clients/twitter_client/harness.go
+++ pkg/clients/twitter_client/harness.go
@@ -4,14 +4,18 @@ import (
 	"github.com/dghubble/go-twitter/twitter"
 )
 
+func CreateTwitterTestClient(h TwitterHarness) TwitterTestClient {
+	return TwitterTestClient{h}
+}
+
 type TwitterTestClient struct {
 	harness TwitterHarness
 }
 
 func (ttc TwitterTestClient) Search(s string, p *twitter.SearchTweetParams) (*twitter.Search, error) {
-	return ttc.harness.search(s, p)
+	return ttc.harness.Search(s, p)
 }
 
 type TwitterHarness struct {
-	search func(s string, p *twitter.SearchTweetParams) (*twitter.Search, error)
+	Search func(s string, p *twitter.SearchTweetParams) (*twitter.Search, error)
 }
