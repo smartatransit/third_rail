f1aff73025d0198b54cbfba6e1402a0b748b212b
Bipol Alam <bipolalam@gmail.com>
2020-11-16 17:52:06 -0500

    initial implementation

--- pkg/controllers/admin.go
+++ pkg/controllers/admin.go
@@ -2,16 +2,30 @@ package controllers
 
 import (
 	"encoding/json"
+	"net/http"
+
 	"github.com/jinzhu/gorm"
 	log "github.com/sirupsen/logrus"
 	"github.com/smartatransit/gomarta"
 	"github.com/smartatransit/third_rail/pkg/clients"
-	"net/http"
 )
 
 type AdminController struct {
 	MartaClient clients.MartaClient
 	AdminKey    string
+}
+
+//HealthResponse represents a response to the health-check endpoint
+type HealthResponse struct {
+	Statuses []Status `json:"statuses"`
+}
+
+//Status represents a single system status
+type Status struct {
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	Healthy     bool        `json:"healthy"`
+	Metadata    interface{} `json:"metadata,omitempty"`
 }
 
 func (ac AdminController) checkAdmin(key string) bool {
@@ -37,4 +51,46 @@ func (ac AdminController) IngestEvent(db *gorm.DB, w http.ResponseWriter, req *h
 	var resp Response
 
 	json.NewEncoder(w).Encode(resp)
+}
+
+//Health responds with a variety of internal statuses
+func (ac AdminController) Health(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	var statuses []Status
+	defer func() {
+		if len(statuses) == 0 {
+			statuses = append(statuses, Status{
+				Name:        "database",
+				Description: "postgres backend",
+				Healthy:     false,
+			})
+		}
+
+		ac.writeJSONResponse(w, http.StatusOK, HealthResponse{Statuses: statuses})
+	}()
+
+	_, err := ac.MartaClient.GetTrains()
+	if err != nil {
+		statuses = append(statuses, Status{
+			Name:        "marta_client",
+			Description: err.Error(),
+			Healthy:     false,
+		})
+	}
+
+	statuses = append(statuses, Status{
+		Name:        "database",
+		Description: "postgres backend",
+		Healthy:     true,
+	})
+}
+
+func (ac AdminController) writeJSONResponse(w http.ResponseWriter, status int, body interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	err := json.NewEncoder(w).Encode(body)
+	if err != nil {
+		log.Errorf("failed writing response: %s", err.Error())
+		return
+	}
 }
